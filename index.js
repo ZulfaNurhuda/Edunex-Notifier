5cd118722bc5015db60b75f6f6f437b1f44caf69
ZulfaNurhuda <afluzkeren@gmail.com>
2024-09-22 22:07:37 +0700

    Database Connection Fix
    Mengupdate script supaya terhubung dengan database di MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config({ path: `${process.cwd()}/.env`});
 const handler = require(`./Utility/handler`);
+const database = require(`./Utility/database`);
 
-handler()
-.then(() => {
-    console.log(`System is Ready!`)
-    setInterval(() => {
-        handler().catch(e => console.error(`X | Error: ${e}`));
-    }, 1000 * 60 * 5)
+database().then(() => {
+    handler().then(() => {
+        console.log(`V | System is Ready!`)
+        setInterval(() => {
+            handler().catch(e => console.error(`X | Error: ${e}`));
+        }, 1000 * 60 * 5)
+    })
+    .catch(e => console.error(`X | Error: ${e}`));
 })
 .catch(e => console.error(`X | Error: ${e}`));
 
